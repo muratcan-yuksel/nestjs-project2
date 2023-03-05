bba48363db331e895082734ac4fda5642c8745be
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-05 22:57:14 +0300

    working on jwt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,63 +1,76 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
   async signup(dto: AuthDto) {
-    //generate the password hash
+    // generate the password hash
     const hash = await argon.hash(dto.password);
-    //this try catch error handling didn't work at all
+    // save the new user in the db
     try {
-      //save the new user in the db
-      const user: User = await this.prisma.user.create({
+      const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           hash,
         },
       });
-      //delete user hash from return statement
-      delete user.hash;
-      return user;
+
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
-        //this code means that "you're trying to create a user with an email that already exists"
         if (error.code === 'P2002') {
-          throw new ForbiddenException('Email already exists');
+          throw new ForbiddenException('Credentials taken');
         }
       }
-      //if it doesn't come from prisma
       throw error;
     }
   }
 
   async signin(dto: AuthDto) {
-    //find user by email
-    //if user does not exist throw error
+    // find the user by email
     const user = await this.prisma.user.findUnique({
       where: {
         email: dto.email,
       },
     });
-    if (!user) {
-      throw new ForbiddenException('Invalid credentials');
-    }
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
 
-    //compare password
-    //if password is wrong throw error
+    // compare password
     const pwMatches = await argon.verify(user.hash, dto.password);
-    if (!pwMatches) {
-      throw new ForbiddenException('Invalid credentials');
-    }
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+    return this.signToken(user.id, user.email);
+  }
 
-    //send back the user
-    delete user.hash;
-    return user;
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
 
-    return { msg: 'hi' };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
